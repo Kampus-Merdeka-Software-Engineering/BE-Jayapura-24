a8e0823749d60d062a8acb9ebffa1fe1f27ddf68
Rizal <rizalfanani510@gmail.com>
2023-10-06 17:55:08 +0700

    commit

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 const bodyParser = require('body-parser');
 const {Comment} = require('./models');
+const cors = require('cors')
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(cors({origin:"*"}))
 
 //menyimpan data
 app.post('/comment', async (req, res) => {
